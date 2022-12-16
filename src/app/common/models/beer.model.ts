0b9e19c87c28b0fed23b994bab88eac47ad60cd1
sasha <alexanderdymshyts@gmail.com>
2022-12-16 13:45:49 +0100

    add beer model.ts

---
+++ src/app/common/models/beer.model.ts
@@ -0,0 +1,8 @@
+export class Beer{
+    id: number | undefined;
+    description: string | undefined;
+    name: string | undefined;
+    currency: string | undefined;
+    imageUrl: string | undefined;
+    price: number | undefined;
+}
