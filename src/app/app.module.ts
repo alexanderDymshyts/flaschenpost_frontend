fec4c54c32b22badc78fdaa6fe87b9b04783d711
sasha <alexanderdymshyts@gmail.com>
2022-12-16 13:30:59 +0100

    added bootstrap 5.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
+import "bootstrap";
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+
 
 @NgModule({
   declarations: [
