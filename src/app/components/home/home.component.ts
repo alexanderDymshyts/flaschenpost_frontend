37982391a244ebd0a491111320698e3280e28cb7
sasha <alexanderdymshyts@gmail.com>
2022-12-17 19:51:18 +0100

    Home component extends HomeState

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,11 +7,13 @@ import { GlobalBeerState, GLOBAL_BEER_STATE, HomeState } from "src/app/common/st
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent extends RxState<HomeState> implements OnInit{
 
     public beersToShow$ = this.globalState.select('allBeers');
 
-    constructor(@Inject(GLOBAL_BEER_STATE) private globalState: RxState<GlobalBeerState>){}
+    constructor(@Inject(GLOBAL_BEER_STATE) private globalState: RxState<GlobalBeerState>){
+        super();
+    }
     
     ngOnInit(): void {                   
     }    
